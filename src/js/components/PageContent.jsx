b22a170990a265af43cdda35bce858e4508c98a5
JosueJMartinez <jmarti61@gmail.com>
2020-11-15 21:27:52 -0600

    removed width property to get rid of horizontal scroll

--- src/js/components/PageContent.jsx
+++ src/js/components/PageContent.jsx
@@ -3,7 +3,6 @@ import React from "react";
 export default function PageContent(props) {
   const styles = {
     height: "100vh",
-    width: "100vw",
   };
   return <div style={styles}>{props.children}</div>;
 }
